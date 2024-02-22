25a80ddf4d98400e4521102e878e2061f80839bc
AlbertZumaya <156123337+AlbertZumaya@users.noreply.github.com>
2024-02-22 11:21:36 -0600

    Getting ready to set up my logic for the Dashing Speed mechanic

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (!(false)) {
+    	
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (!(false)) {
+    	
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (!(false)) {
+    	
+    }
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (!(false)) {
+    	
+    }
+})
 tiles.setCurrentTilemap(tilemap`level2`)
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
